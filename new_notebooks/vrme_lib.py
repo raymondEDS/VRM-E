97825631fcd83f8015392511cfc6438acb94be76
Neha Kennard <nnayak@umass.edu>
2023-05-20 16:53:48 -0400

    tidying

--- new_notebooks/vrme_lib.py
+++ new_notebooks/vrme_lib.py
@@ -1,4 +1,5 @@
 import ast
+import pandas as pd
 
 
 def print_results(name, atc, ci_lower_bound, ci_upper_bound):
@@ -8,12 +9,29 @@ def print_results(name, atc, ci_lower_bound, ci_upper_bound):
     print(f'{name} CI range: {abs(ci_lower_bound) -abs(ci_upper_bound):.4}')
     
     
-    
-    
-    
 def get_numpy (row):
     return ast.literal_eval(row.embedding)
 
+
+def load_data():
+    embedding_path = '../data/raw/df_embeddings.csv'
+    submission_path = '../data/raw/df_submission_rating.csv'
+
+    df_embeddings = pd.read_csv(embedding_path).T
+    df_embeddings.columns=df_embeddings.iloc[0]
+    df_embeddings = df_embeddings.iloc[1: , :]
+
+    df_embeddings['embedding'] = df_embeddings.apply(lambda x: get_numpy(x), axis =1)
+
+    df_submissions = pd.read_csv(submission_path)
+    df_submission_labels = df_submissions[['id','title','conf_year','keywords','AVG_rating']]
+
+    df_embeddings_2017 = df_embeddings.merge(df_submission_labels[df_submission_labels['conf_year']==2017], left_on='paper_id',right_on='id')
+    df_embeddings_2018 = df_embeddings.merge(df_submission_labels[df_submission_labels['conf_year']==2018], left_on='paper_id',right_on='id')
+    assert df_submission_labels[df_submission_labels['conf_year']==2017].shape[0] == df_embeddings_2017.shape[0]
+    
+    
+    return df_embeddings, df_submissions, df_submission_labels, df_embeddings_2017, df_embeddings_2018
 
 
 # ==== SPSM utils ================================================================================
@@ -56,3 +74,10 @@ def strip_char(row):
     for word in word_list:
         s += " " + word.replace(' ','_').replace('-','_').lower()
     return s
+
+# ==== Agreement utils ===========================================================================
+def print_ttest(other_method_name, ttest):
+    print(f'T-Test: {other_method_name} vs VRM-E')
+    print(f'statistic: {ttest.statistic:.4}')
+    print(f'p-value: {ttest.pvalue}')
+    print()
